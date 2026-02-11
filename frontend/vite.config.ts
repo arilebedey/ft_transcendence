f094678657765c6b675aed1263bdf1ef79c1696e
cuistobal <aled@gmail.com>
2026-02-11 08:30:45 +0100

    Works but night mode

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,5 @@
 import { defineConfig } from "vite";
-<<<<<<< HEAD
 import react from "@vitejs/plugin-react-swc";
-=======
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
->>>>>>> 2c426df (Modifications packages -> making sure we work with the right deps. Modified colors values -> switched to hex)
 import path from "path";
 
 // https://vitejs.dev/config/
@@ -17,11 +12,7 @@ export default defineConfig(({ mode }) => ({
       },
     },
   },
-<<<<<<< HEAD
   plugins: [react()],
-=======
-  plugins: [react(), tailwindcss()],
->>>>>>> 2c426df (Modifications packages -> making sure we work with the right deps. Modified colors values -> switched to hex)
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
