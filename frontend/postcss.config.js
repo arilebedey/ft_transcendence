5dd45bfa163d0e38b27965e78ca2172cff39916d
cuistobal <aled@gmail.com>
2026-02-11 07:59:26 +0100

    Works but ugly af

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -1,10 +1,6 @@
 export default {
   plugins: {
-<<<<<<< HEAD
-    "@tailwindcss/postcss": {},
-=======
     tailwindcss: {},
     autoprefixer: {},
->>>>>>> 6b6eef6 (Add front-end proposition. Working on plugging back-end)
   },
 };
