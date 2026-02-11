0a8e4346e7aff2ce54fae5e8899bca8e19cae847
cuistobal <aled@gmail.com>
2026-02-11 19:00:57 +0100

    A 2 doigts de me foutre en l'air

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 import { type ClassValue, clsx } from "clsx";
-=======
-import { clsx, type ClassValue } from "clsx";
->>>>>>> 6b6eef6 (Add front-end proposition. Working on plugging back-end)
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
