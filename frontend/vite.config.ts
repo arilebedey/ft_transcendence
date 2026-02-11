a1806009dac84bb333b05594a5f8ac1010d26b55
cuistobal <aled@gmail.com>
2026-02-11 07:50:32 +0100

    Modifications packages -> making sure we work with the right deps. Modified colors values -> switched to hex

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,5 +1,10 @@
 import { defineConfig } from "vite";
+<<<<<<< HEAD
 import react from "@vitejs/plugin-react-swc";
+=======
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+>>>>>>> 2c426df (Modifications packages -> making sure we work with the right deps. Modified colors values -> switched to hex)
 import path from "path";
 
 // https://vitejs.dev/config/
@@ -12,7 +17,11 @@ export default defineConfig(({ mode }) => ({
       },
     },
   },
+<<<<<<< HEAD
   plugins: [react()],
+=======
+  plugins: [react(), tailwindcss()],
+>>>>>>> 2c426df (Modifications packages -> making sure we work with the right deps. Modified colors values -> switched to hex)
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
