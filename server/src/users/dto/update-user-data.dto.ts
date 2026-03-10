a6a6aabc45c1cdef9fde546982754a163c3c7a28
theaux42 <tbabou@student.42.fr>
2026-03-10 14:47:40 +0100

    fix: add of username's check in DTO

--- server/src/users/dto/update-user-data.dto.ts
+++ server/src/users/dto/update-user-data.dto.ts
@@ -1,12 +1,23 @@
-import { IsIn, IsOptional, IsString } from 'class-validator';
+import {
+  IsIn,
+  IsOptional,
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 
 export class UpdateUserDataDto {
   @IsOptional()
   @IsString()
+  @MinLength(3)
+  @MaxLength(12)
+  @Matches(/^[a-z0-9][a-z0-9_]*$/)
   name?: string;
 
   @IsOptional()
   @IsString()
+  @MaxLength(160)
   bio?: string;
 
   @IsOptional()
