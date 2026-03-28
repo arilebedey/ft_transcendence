f9742523461d2b6d3a12e9b9015c260891c3ef35
arilebedey <34816154+arilebedey@users.noreply.github.com>
2026-03-28 18:50:06 +0100

    fix: search all existing users + improve

--- server/src/users/users.controller.ts
+++ server/src/users/users.controller.ts
@@ -31,6 +31,11 @@ export class UsersController {
     return this.userDataService.get(user.id);
   }
 
+  @Get('search')
+  async search(@GetUser() user: AuthUser, @Query('q') query?: string) {
+    return this.userDataService.search(user.id, query);
+  }
+
   @Patch('me')
   async updateMe(@GetUser() user: AuthUser, @Body() dto: UpdateUserDataDto) {
     return this.userDataService.update(user.id, dto);
